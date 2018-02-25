51b787ffedc77657da09c802d8b6fe8f77c6aa3d
nicolehardt <nico8399@edu.easj.dk>
2018-02-25 19:17:59 +0100

    Facebook desktop-version ordnet

--- jquery.js
+++ jquery.js
@@ -1,23 +1,23 @@
 window.onload = function(){
-  alert('welcome');
+  //alert('welcome');
 };
 
 $(document).ready(function(){
   $('#facebook').click (function(){
     $('#facebookframe').slideToggle('slow');
-    $('#portrait').toggle('slow');
+    //$('#portrait').toggle('slow');
     $('#instagram').toggle('slow');
     $('#youtube').toggle('slow');
   });
   $('#instagram').click (function() {
     $('#instagramframe').slideToggle('slow')
-    $('#portrait').toggle('slow');
+    //$('#portrait').toggle('slow');
     $('#facebook').toggle('slow');
     $('#youtube').toggle('slow');
   });
   $('#youtube').click(function () {
     $('#youtubeframe').slideToggle('slow');
-    $('#portrait').toggle('slow');
+    //$('#portrait').toggle('slow');
     $('#instagram').toggle('slow');
     $('#facebook').toggle('slow');
   });
