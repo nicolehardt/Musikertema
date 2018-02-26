0175a1b56911351d75ad253ba44d61a70383a6b0
nicolehardt <nico8399@edu.easj.dk>
2018-02-26 13:05:21 +0100

    Spotify indsat

--- jquery.js
+++ jquery.js
@@ -1,24 +1,40 @@
-window.onload = function(){
+/*window.onload = function(){
   //alert('welcome');
-};
+};*/
 
 $(document).ready(function(){
   $('#facebook').click (function(){
     $('#facebookframe').slideToggle('slow');
-    //$('#portrait').toggle('slow');
+
+    console.log(window.width);
+/*
+    if(window.width < 400){
+    $('#portrait').toggle('slow');
+  }*/
+
     $('#instagram').toggle('slow');
+    $('#spotify').toggle('slow');
     $('#youtube').toggle('slow');
   });
   $('#instagram').click (function() {
     $('#instagramframe').slideToggle('slow')
     //$('#portrait').toggle('slow');
     $('#facebook').toggle('slow');
+    $('#spotify').toggle('slow');
     $('#youtube').toggle('slow');
+  });
+  $('#spotify').click(function () {
+    $('#spotifyframe').slideToggle('slow');
+    //$('#portrait').toggle('slow');
+    $('#instagram').toggle('slow');
+    $('#youtube').toggle('slow');
+    $('#facebook').toggle('slow');
   });
   $('#youtube').click(function () {
     $('#youtubeframe').slideToggle('slow');
     //$('#portrait').toggle('slow');
     $('#instagram').toggle('slow');
+    $('#spotify').toggle('slow');
     $('#facebook').toggle('slow');
   });
 });
