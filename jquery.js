0a1d2311c415c6385622316fee9c884fd2fe9e16
nicolehardt <nico8399@edu.easj.dk>
2018-02-28 14:57:20 +0100

    Gammel jquery slettet

--- jquery.js
+++ jquery.js
@@ -1,62 +1,18 @@
-/*window.onload = function(){
-  //alert('welcome');
-};*/
-
-
-/*
-$(document).ready(function(){
-  $('#facebook').click (function(){
-    $('#facebookframe').slideToggle('slow');
-    $('#portrait').toggle('slow');
-    $('#instagram').toggle('slow');
-    $('#spotify').toggle('slow');
-    $('#youtube').toggle('slow');
-  });
-  $('#instagram').click (function() {
-    $('#instagramframe').slideToggle('slow')
-    //$('#portrait').toggle('slow');
-    $('#facebook').toggle('slow');
-    $('#spotify').toggle('slow');
-    $('#youtube').toggle('slow');
-  });
-  $('#spotify').click(function () {
-    $('#spotifyframe').slideToggle('slow');
-    //$('#portrait').toggle('slow');
-    $('#instagram').toggle('slow');
-    $('#youtube').toggle('slow');
-    $('#facebook').toggle('slow');
-  });
-  $('#youtube').click(function () {
-    $('#youtubeframe').slideToggle('slow');
-    //$('#portrait').toggle('slow');
-    $('#instagram').toggle('slow');
-    $('#spotify').toggle('slow');
-    $('#facebook').toggle('slow');
-  });
-});
-*/
-
 $(document).ready(function(){
   $('#overlay').click (function () {
     $('#overlay').hide('slow');
   });
   $('#burger').click (function() {
-    //$('nav').removeClass('burgermenu');
     $('nav').show('slow');
     $('#cross').show('slow');
     $('#burger').hide();
-    //$('#freija').hide();
-    //$('.logo').hide();
-    //$('ul').addClass('fjernmargin');
     $('header').addClass('burgerheader');
   });
   $('#cross').click (function() {
-    //$('nav').addClass('burgermenu');
     $('nav').hide('slow');
     $('#burger').show('slow');
     $('#cross').hide();
-    //$('#freija').show();
-    //$('.logo').show();
+
   });
   $('.navforsvind').click (function () {
     $('nav').hide('slow');
